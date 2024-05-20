18f5f92ab9094c8b8c980e497110f9b6fd052039
RenzoZumaeta98 <renzozumaeta98@gmail.com>
2024-05-19 23:32:56 -0500

    1er Commit Subiendo Proyecto Manejo Presupuesto Con 1er Controller

---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")] //{0} Para que aparezca "Nombre" en el mensaje
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1}")] //Para numero de caracteres
+        [Display(Name = "Nombre del tipo cuenta")]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+
+
+
+    }
+}
