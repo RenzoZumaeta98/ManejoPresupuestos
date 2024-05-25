80b5ff55d06a005961a98e49c36115860745d0a3
RenzoZumaeta98 <renzozumaeta98@gmail.com>
2024-05-25 00:10:52 -0500

    Agragando metodo crear en TiposCuentasController con buenas practicas ASYNC - AWAIT

--- ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -1,5 +1,6 @@
 ﻿using Dapper;
 using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 
@@ -8,20 +9,32 @@ namespace ManejoPresupuesto.Controllers
     public class TiposCuentasController: Controller 
     {
 
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+
+        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas)
+        {
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+        }
+
+
         public IActionResult Crear()
         {
-                return View();
+            return View();
         }
 
         [HttpPost]
-        public IActionResult Crear(TipoCuenta tipoCuenta)
+        public async Task<IActionResult> Crear(TipoCuenta tipoCuenta)
         {
             if (!ModelState.IsValid)
             {
                 return View(tipoCuenta);
             }
+
+            tipoCuenta.UsuarioId = 1;
+            await repositorioTiposCuentas.Crear(tipoCuenta);
             return View();
         }
+
 
     }
 }
---
+++ ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioTiposCuentas
+    {
+        Task Crear(TipoCuenta tipoCuenta);
+        
+    }
+    public class RepositorioTiposCuentas : IRepositorioTiposCuentas
+    {
+        private readonly string connectionString;
+        public RepositorioTiposCuentas(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        }
+
+        public async Task Crear(TipoCuenta tipoCuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>($@"INSERT INTO dbo.TiposCuentas(Nombre,UsuarioId,Orden)
+                                                Values (@Nombre, @UsuarioId,0);
+                                                SELECT SCOPE_IDENTITY();", tipoCuenta);
+            tipoCuenta.Id = id;
+        }
+
+    }
+}
+
